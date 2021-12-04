f6719588702929c99db9d46642328cb999e21047
Popcorn-0924 <73300441+Popcorn-0924@users.noreply.github.com>
2021-12-04 23:44:12 +0800

    Delete serve_round1 directory

--- serve_round1/label.py
+++
@@ -1,8 +0,0 @@
-file = open('test_serve_Y_round1.txt', 'a')
-line = input("Label No: ")
-num = int(int(input("How much this label will be print? ")))
-print(num)
-for i in range(num):
-    tmp = int(line)
-    file.write(str(tmp)+"\n")
-file.close()
